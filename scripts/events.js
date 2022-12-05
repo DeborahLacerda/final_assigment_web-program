486ef1eac10a2612262be6ff64eed4b99589f4af
Deborah Lacerda <lace0115@algonquinlive.com>
2022-12-04 21:21:42 -0500

    code cleanup and squashing bugs

--- scripts/events.js
+++ scripts/events.js
@@ -3,54 +3,11 @@
 
 //form element events
 
-///////remove warning////
-
-//remove warning if dName has input
-document.querySelector("#dname").addEventListener("blur", function () {
-  const textbox = document.getElementById("dname");
-  if (textbox.value.length <= 10 && textbox.value.length >= 5) {
-    dnameError.textContent = "";
-  }
-});
-
-//remove warning if age has input
-document.querySelector("#quantity").addEventListener("blur", function () {
-  const textbox = document.getElementById("quantity");
-  if (textbox.value <= 10 && textbox.value >= 5) {
-    quantityError.textContent = "";
-  }
-});
-
-document.querySelector("#date").addEventListener("blur", function () {
-  const monthIndex = 1;
-  const today = new Date(2022, 10, 29);
-  const day = document.getElementById("date");
-  const dateParts = day.value.split("-").map((datePart, index) => {
-    let value = parseInt(datePart);
-    if (index === monthIndex) {
-      value -= 1; // because months are base 0.... for reasons
-    }
-    return value;
-  });
-  let userDate = new Date(...dateParts);
-
-  if (userDate > today) {
-    dateError.textContent = "";
-  }
-});
-
 //call the funtion validateProfile
-
-document.profile.addEventListener("submit", validateProfile);
+document.profile.addEventListener("submit", submitProfile);
 
 //call the funtion clearAll
-
 document.profile.addEventListener("reset", clearAll);
 
 /// any change
-
-document.profile.addEventListener("change", validateEye);
-document.profile.addEventListener("change", validateDate);
-document.profile.addEventListener("change", validateColor);
-document.profile.addEventListener("change", validateDName);
-document.profile.addEventListener("change", validateQuantity);
+document.profile.addEventListener("change", validateProfile);
--- scripts/validate.js
+++ scripts/validate.js
@@ -2,41 +2,59 @@
 //lace0115@algonquinlive.com
 
 //validate module
-function validateProfile(e) {
-  // e.preventDefault();
 
-  validateDName();
-  validateQuantity();
-  validateDate();
-  validateColor();
-  validateEye();
+function submitProfile(e) {
+  const isFormValid = validateProfile();
+
+  if (!isFormValid) {
+    e.preventDefault();
+  }
+}
+
+function validateProfile() {
+  const isNameValid = validateDName();
+  const isQuantityValid = validateQuantity();
+  const isDateValid = validateDate();
+  const isColorValid = validateColor();
+  const isEyeValid = validateEye();
+
+  return (
+    isNameValid && isQuantityValid && isDateValid && isColorValid && isEyeValid
+  );
 }
 
 ///a) Text (min and max length)
 
 function validateDName() {
   const textbox = document.getElementById("dname");
-  if (textbox.value.length <= 10 && textbox.value.length >= 2) {
-  } else if (dname.value === "") {
+  let isValid = true;
+  if (textbox.value.length > 10 || textbox.value.length < 2) {
     document.querySelector("#dnameError").textContent =
       "*Please make sure the Name is between 2-10 characters long*";
+    isValid = false;
+  } else {
+    document.querySelector("#dnameError").textContent = "";
   }
-  document.querySelector("#dnameError").textContent =
-    "*Please make sure the Name is between 2-10 characters long*";
+  return isValid;
 }
 
 ///b) Number (min and max of your choice)
 
 function validateQuantity() {
-  const textbox = document.getElementById("quantity");
-  if (textbox.value <= 10 && textbox.value >= 2) {
-  } else if (quantity.value === "") {
+  const quantityField = document.getElementById("quantity");
+  let isValid = true;
+  if (
+    quantityField.value === "" ||
+    quantityField.value > 10 ||
+    quantityField.value < 2
+  ) {
     document.querySelector("#quantityError").textContent =
       "*Please enter a quantity between 2 and 10.*";
+    isValid = false;
   } else {
-    document.querySelector("#quantityError").textContent =
-      "*Please enter a quantity between 2 and 10.*";
+    document.querySelector("#quantityError").textContent = "";
   }
+  return isValid;
 }
 
 ///c) Date ( verify input to be after specific date
@@ -44,6 +62,7 @@ function validateQuantity() {
 function validateDate() {
   const monthIndex = 1;
   const today = new Date(2022, 10, 29);
+  let isValid = true;
 
   const day = document.getElementById("date");
   const dateParts = day.value.split("-").map((datePart, index) => {
@@ -55,46 +74,48 @@ function validateDate() {
     return value;
   });
 
-  // let userDate = new Date(...dateParts);
   let userDate = new Date(dateParts[0], dateParts[1], dateParts[2]);
 
-  if (userDate <= today) {
+  if (day.value === "" || userDate <= today) {
     document.querySelector("#dateError").textContent =
-      "*Please make sure the input is date after 29-Nov-2022*";
-  } else if (day.value === "") {
-    document.querySelector("#dateError").textContent =
-      "*Please make sure the input is date after 29-Nov-2022*";
+      "*Please pick a date after 29-Nov-2022*";
+    isValid = false;
   } else {
     document.querySelector("#dateError").textContent = "";
   }
+
+  return isValid;
 }
 
 ///d) A List that allows editing input and single selection (Required)
 
 function validateColor() {
   var ccolor = document.getElementById("color").value;
+  let isValid = true;
   if (ccolor === "") {
     document.querySelector("#colorError").textContent =
-      "*Please make sure the input the color*";
+      "*Please input the color*";
+    isValid = false;
   } else {
     document.querySelector("#colorError").textContent = "";
   }
+  return isValid;
 }
 
 //check radio was check
 
 function validateEye() {
   var eyes = document.profile.eyes.value; // chech is this has value or not
+  let isValid = true;
   if (eyes === "") {
     document.querySelector("#eyesError").textContent =
-      "*Please make sure the input the color*";
+      "*Please select the eye type*";
+    isValid = false;
   } else {
     document.querySelector("#eyesError").textContent = "";
   }
+  return isValid;
 }
-
-const elements1 = document.querySelectorAll('[name="eyes"]');
-console.log(elements1);
 
 function clearAll(e) {
   document.querySelector("#dnameError").textContent = "";
